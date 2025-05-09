1062ab266e2bd6026ad7f9998355efd2b2267b08
VCarames <vcarames-dev@outlook.com>
2025-05-08 23:10:59 -0700

    "Benefit Block" and "Benefit" Section Updated For Improved Styling

--- src/components/BenefitBlock.jsx
+++ src/components/BenefitBlock.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-const BenefitBlock = ({ image, heading, text, link, linkText }) => {
+const BenefitBlock = ({ className, image, heading, text, link, linkText }) => {
   return (
-    <div className="benefits-block__content">
+    <div className={`benefits-block ${className || ""}`}>
       {image && <img className="benefits-block__img" src={image} alt="" />}
-      <h3 className="benefits-block__heading">{heading}</h3>
-      <p className="benefits-block__text">{text}</p>
-      <a href={link} className="benefits-block__btn button">
-        {linkText}
-      </a>
+      <div className="benefits-block__content">
+        <h3 className="benefits-block__heading">{heading}</h3>
+        <p className="benefits-block__text">{text}</p>
+        <a href={link} className="benefits-block__btn button">
+          {linkText}
+        </a>
+      </div>
     </div>
   );
 };
--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -5,38 +5,38 @@ import BenefitsImgThree from "/src/assets/images/payments.jpg";
 
 function Benefits() {
   return (
-    <section className="benefits__section">
+    <section className="benefits-section">
       <h2 className="visually-hidden">Why Use Sccot?</h2>
+
       <div className="benefits-section__pattern benefits-section__pattern--one">
-        <div className="">
-          <BenefitBlock
-            image={BenefitsImgOne}
-            heading="Zero hassle payments"
-            text="Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month."
-            link="/"
-            linkText="Learn More"
-          />
-        </div>
+        <BenefitBlock
+          image={BenefitsImgOne}
+          heading="Easy to use riding telemetry"
+          text="Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month."
+          link="/"
+          linkText="Learn More"
+        />
+      </div>
 
-        <div className="benefits-section__pattern benefits-section__pattern--two">
-          <BenefitBlock
-            image={BenefitsImgTwo}
-            heading="Coming to a city near you"
-            text="Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year."
-            link="/"
-            linkText="Learn More"
-          />
-        </div>
+      <div className="benefits-section__pattern benefits-section__pattern--two">
+        <BenefitBlock
+          className="benefits-block--alt"
+          image={BenefitsImgTwo}
+          heading="Coming to a city near you"
+          text="Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your hometown. We’re aiming to let our scooters loose on 23 cities over the coming year."
+          link="/"
+          linkText="Learn More"
+        />
+      </div>
 
-        <div className="benefits-section__pattern benefits-section__pattern--three">
-          <BenefitBlock
-            image={BenefitsImgThree}
-            heading="Zero hassle payments"
-            text="Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month."
-            link="/"
-            linkText="Learn More"
-          />
-        </div>
+      <div className="benefits-section__pattern benefits-section__pattern--three">
+        <BenefitBlock
+          image={BenefitsImgThree}
+          heading="Zero hassle payments"
+          text="Our payment is as easy as one two three. We accept most credit cards and debit cards. You can also link your PayPal account inside the app. Need to pay later? No worries! You can defer payment for up to a month."
+          link="/"
+          linkText="Learn More"
+        />
       </div>
     </section>
   );
