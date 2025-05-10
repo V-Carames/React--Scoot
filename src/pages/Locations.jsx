f9c03c50b0b9e337f520c729ff9c131999d80238
VCarames <vcarames-dev@outlook.com>
2025-05-10 16:52:32 -0700

    "Headquarters" Section Imported to "Locations" Page

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,9 +1,11 @@
 import IntroBlock from "../components/blocks/IntroBlock";
+import Headquarters from "../sections/Headquarters";
 
 function Locations() {
   return (
     <>
       <IntroBlock className="intro-block--locations" heading="Locations" />
+      <Headquarters />
     </>
   );
 }
