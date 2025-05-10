e2f2a8837fb68b112128add5244488ad85bf8859
VCarames <vcarames-dev@outlook.com>
2025-05-10 10:44:07 -0700

    "Benefit Block" Conditional Update

--- src/components/blocks/BenefitBlock.jsx
+++ src/components/blocks/BenefitBlock.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const BenefitBlock = ({ className, image, heading, text, link, linkText }) => {
+const BenefitBlock = ({
+  className = "",
+  image,
+  heading,
+  text,
+  link,
+  linkText,
+}) => {
   return (
-    <div className={`benefits-block ${className || ""}`}>
+    <div className={`benefits-block ${className}`}>
       {image && <img className="benefits-block__img" src={image} alt="" />}
       <div className="benefits-block__content">
         <h3 className="benefits-block__heading">{heading}</h3>
         <p className="benefits-block__text">{text}</p>
-        <a href={link} className="benefits-block__btn button">
-          {linkText}
-        </a>
+
+        {link && linkText && (
+          <Link to={link} className="benefits-block__btn button">
+            {linkText}
+          </Link>
+        )}
       </div>
     </div>
   );
