59aacebcc361377fcf67ec2c3521a2bc1a71bdf6
VCarames <vcarames-dev@outlook.com>
2025-05-10 10:45:04 -0700

    "Benefit Block" Added to "About" and Careers" Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,35 @@
 import IntroBlock from "../components/blocks/IntroBlock";
+import BenefitBlock from "../components/blocks/BenefitBlock";
+import BenefitsImgFour from "/src/assets/images/digital-era.jpg";
+import BenefitsImgFive from "/src/assets/images/better-living.jpg";
 
 function About() {
   return (
     <>
       <IntroBlock className="intro-block--about" heading="About" />
+      <section className="benefits-section">
+        <h2 className="visually-hidden">Benefits of Using Scoot</h2>
+        <div className="benefits-section__pattern benefits-section__pattern--four">
+          <BenefitBlock
+            image={BenefitsImgFour}
+            heading="Mobility for the digital era"
+            text=" Getting around should be simple (and even fun!) for everyone. We embrace technology to provide low cost, smart access to scooters at your fingertips."
+            link=""
+            linkText=""
+          />
+        </div>
+
+        <div className="benefits-section__pattern benefits-section__pattern--five">
+          <BenefitBlock
+            className="benefits-block--alt"
+            image={BenefitsImgFive}
+            heading="Better urban living"
+            text="We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride."
+            link=""
+            linkText=""
+          />
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,9 +1,23 @@
 import IntroBlock from "../components/blocks/IntroBlock";
+import BenefitBlock from "../components/blocks/BenefitBlock";
+import BenefitsImgSix from "/src/assets/images/join-us.jpg";
 
 function Careers() {
   return (
     <>
       <IntroBlock className="intro-block--careers" heading="Careers" />
+      <section className="benefits-section ">
+        <h2 className="visually-hidden">Join Us</h2>
+        <div className="benefits-section__pattern benefits-section__pattern--six">
+          <BenefitBlock
+            image={BenefitsImgSix}
+            heading="Care to join our mission?"
+            text=" We’re always looking for ambitious individuals to help us on our journey. If you’re passionate about our mission to provide clean, accessible transport to improve urban living we want to hear from you!"
+            link="/"
+            linkText="Say Hello"
+          />
+        </div>
+      </section>
     </>
   );
 }
