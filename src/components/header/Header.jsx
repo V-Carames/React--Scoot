3d2e978e2fa6df6739c80e1f6aba29fbe5cb8cce
VCarames <vcarames-dev@outlook.com>
2025-05-10 17:16:25 -0700

    Scroll Detection Added to "Header"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsScrolled(window.scrollY > 0);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${isScrolled ? "scrolled" : ""}`}>
       <div className="header__container">
         <HeaderLogo />
         <HeaderNav />
