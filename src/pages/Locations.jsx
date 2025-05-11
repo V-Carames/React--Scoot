717f4bada48cbcd422d43f569be106950c4a63b5
VCarames <vcarames-dev@outlook.com>
2025-05-10 17:07:08 -0700

    "Not Listed" Imported to "Locations" Page

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,11 +1,13 @@
 import IntroBlock from "../components/blocks/IntroBlock";
 import Headquarters from "../sections/Headquarters";
+import NotListed from "../sections/NotListed";
 
 function Locations() {
   return (
     <>
       <IntroBlock className="intro-block--locations" heading="Locations" />
       <Headquarters />
+      <NotListed />
     </>
   );
 }
